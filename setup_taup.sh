35f783327130812d4387416c9dae9afda1ca4261
sagitta42 <you@example.com>
2019-07-25 16:24:42 +0200

    Setup includes generator

--- setup_taup.sh
+++ setup_taup.sh
@@ -5,3 +5,7 @@ ln -s MCfits/input_files .
 ln -s MCfits/pdfs_TAUP2017 .
 ## SPECIES LISTS FOR MV
 cp -i MCfits/bin/* bin/.
+## generator
+cp MCfits/generator.py .
+cp MCfits/creator.py .
+ln -s MCfits/templates .
