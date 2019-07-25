cd7ac3f330e871638ac3d0ad872e034682d0170d
sagitta42 <you@example.com>
2019-07-25 13:50:25 +0200

    Quick way to set up TAUP fitoptions and MV species list

---
+++ setup_taup.sh
@@ -0,0 +1,6 @@
+## FITOPTIONS
+# PDFs will be referenced locally in fitoptions
+ln -s /storage/gpfs_data/borexino/users/penek/Simone_MC_31_Jan_2019/mc_fitter_taup/fitter_TAUP/pdfs_TAUP2017 .
+## SPECIES LISTS FOR MV
+cp /storage/gpfs_data/borexino/users/penek/Simone_MC_31_Jan_2019/mc_fitter_taup/fitter_TAUP/bin/multivariate_ps_species_list.icc bin/.
+cp /storage/gpfs_data/borexino/users/penek/Simone_MC_31_Jan_2019/mc_fitter_taup/fitter_TAUP/bin/multivariate_rdist_species_list.icc bin/.
