0f549501ebd53856eab3412c652062e3bbbf0812
sagitta42 <you@example.com>
2019-07-30 14:42:43 +0200

    Simone's yearly PDFs: setup

--- setup_taup.sh
+++ setup_taup.sh
@@ -3,6 +3,7 @@ ln -s MCfits/input_files .
 ## FITOPTIONS
 # PDFs will be referenced locally in fitoptions
 ln -s MCfits/pdfs_TAUP2017 .
+ln -s MCfits/pdfs_years_simone .
 ## SPECIES LISTS FOR MV
 cp -i MCfits/bin/* bin/.
 ## generator
