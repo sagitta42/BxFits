da3d439914a43a232d9c2e5178c9e9bf33aadfe2
sagitta42 <sssagitta@gmail.com>
2019-08-23 15:54:27 +0200

    User input small mods

--- generator.py
+++ generator.py
@@ -2,6 +2,27 @@ import sys
 from creator import * 
 
 #----------------------------------------------------------
+    
+# available values for each parameter
+options = {
+    'CNO': ['fixed','lm', 'hm', 'fixed5'],
+    'var': ['nhits', 'npmts', 'npmts_dt1', 'npmts_dt2'],
+    'fit': ['ene', 'mv'],
+    'inputs': ['all'] + [str(y) for y in range(2012,2018)],
+    'penalty': ICC.keys() + ['pp/pep', 'none'] # penalty not implemented yet
+}
+    
+## defaults
+defaults = {
+    'pdfs': 'pdfs_TAUP2017',
+    'inputs': ['all'], # has to be a list
+    'var': 'nhits',
+}
+    
+## options for user
+user = options.keys() + ['pdfs'] # pdfs is a path to a folder, has no fixed options
+
+#----------------------------------------------------------
 
 def generator(params):
     '''
@@ -19,13 +40,6 @@ def generator(params):
 
     ## ---------------------------------
 
-    # available values for each parameter
-    options = {'CNO': ['fixed','lm', 'hm', 'fixed5'],
-               'var': ['nhits', 'npmts', 'npmts_dt1', 'npmts_dt2'],
-               'fit': ['ene', 'mv'],
-               'inputs': ['all'] + [str(y) for y in range(2012,2018)],
-               'penalty': ICC.keys() + ['pp/pep', 'none']
-    }
 
     # check each parameter given by user
     for par in options:
@@ -70,31 +84,24 @@ def generator(params):
 def main():
     userinput = sys.argv[1:]
     
-    if not len(userinput) in range(3,6):
-        print
-        print 'Examples:'
-        print 'python generator.py CNO=fixed fit=mv var=npmts,nhits'
-        print 'python generator.py fit=ene inputs=2012,2016 var=nhits CNO=fixed5'
-        print 'python generator.py fit=ene var=nhits CNO=fixed5 inputs=2012,2016 pdfs=pdfs_TAUP2017'
-        print
-        sys.exit(1)
+   
+    ## if simpy python generator.py is called, show all options
+    if len(userinput) == 0:
+        wrong_inputs()
 
-    ## options
-    user = ['CNO', 'var', 'fit', 'inputs', 'pdfs','penalty'] # penalty not implemented yet
+    # first assign none
     opts = {}
     for opt in user:
         opts[opt] = 'none'
 
+    # then read what user gave
     for opt in user:
         for inp in sys.argv:
             if opt in inp:
                 opts[opt] = inp.split('=')[1] if opt in ['fit','pdfs'] else inp.split('=')[1].split(',') # penalty can be a list; var is a list to loop on
 
-    ## defaults
-    defaults = {'pdfs': 'pdfs_TAUP2017',
-                'inputs': ['all']
-    }
 
+    # assign defaults if nothing given
     for par in defaults:
         if opts[par] == 'none': opts[par] = defaults[par]
 
@@ -124,6 +131,29 @@ def main():
             for inp in opts['inputs']:
                 params = {'outfolder': outfolder, 'fit': opts['fit'], 'CNO': CNO, 'var': var, 'inputs': str(inp), 'pdfs': opts['pdfs'], 'penalty': opts['penalty']}
                 generator(params)
+
+
+def wrong_inputs():
+    '''A message to print if user input is wrong '''
+    print
+    print 'Parameters:'
+    print ', '.join(user)
+    print
+    print 'Options:'
+    for op in options:
+        print '\t', op, ':', ', '.join(options[op])
+    print 'For inputs, it\'s either "all" or "yyyy,YYYY" (range of years between yyyy and YYYY inclusive)'
+    print
+    print 'Defaults:'
+    for df in defaults:
+        print '\t', df, ':', defaults[df]
+    print
+    print 'Examples:'
+    print 'python generator.py CNO=fixed fit=mv var=npmts,nhits'
+    print 'python generator.py fit=ene inputs=2012,2016 var=nhits CNO=fixed5'
+    print 'python generator.py fit=ene var=nhits CNO=fixed5 inputs=2012,2016 pdfs=pdfs_TAUP2017'
+    print
+    sys.exit(1)
 
 
 if __name__ == '__main__':
