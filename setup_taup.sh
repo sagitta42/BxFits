5781ad65a46fc450cc7bcc47013e7bb5ff1d273e
sagitta42 <you@example.com>
2019-09-23 15:11:47 +0200

    Links instead of copies in the setup not to forget commits

--- setup_taup.sh
+++ setup_taup.sh
@@ -7,6 +7,6 @@ ln -s MCfits/pdfs_years_simone .
 ## SPECIES LISTS FOR MV
 cp -i MCfits/bin/* bin/.
 ## generator
-cp MCfits/generator.py .
-cp MCfits/creator.py .
+ln -s MCfits/generator.py .
+ln -s MCfits/creator.py .
 ln -s MCfits/templates .
